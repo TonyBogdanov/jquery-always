c7dc0eebcee227b6a40554948fb10b64050d0fca
Tony Bogdanov <tonybogdanov@gmail.com>
2018-01-26 14:34:48 +0200

    Updated dependencies

--- bin/rollup-main.sh
+++ bin/rollup-main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ./node_modules/.bin/rollup -c &&\
-./node_modules/.bin/uglifyjs always.js -o always.min.js --mangle --compress --source-map always.min.js.map
+./node_modules/.bin/uglifyjs always.js -o always.min.js --mangle --compress --source-map "url=always.min.js.map"
--- bin/rollup-tests.sh
+++ bin/rollup-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ../node_modules/.bin/rollup -c &&\
-../node_modules/.bin/uglifyjs tests.js -o tests.min.js --mangle --compress --source-map tests.min.js.map
+../node_modules/.bin/uglifyjs tests.js -o tests.min.js --mangle --compress --source-map "url=tests.min.js.map"
